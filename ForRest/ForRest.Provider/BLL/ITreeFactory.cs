085c39d41bca12e819d9fa5ac45ac32e49421564
Piotr Janaszek <janas03@yahoo.pl>
2011-12-08 00:50:27 +0100

    Added ITreeFactory interface
    Interface is used to provide information about plugin and create an instance of a given type.

---
+++ ForRest/ForRest.Provider/BLL/ITreeFactory.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ITreeFactory.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ForRest.Provider.BLL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface ITreeFactory : ITree
+    {
+        string GetPluginName();
+        string GetPluginDescription();
+        ITree GetTree(Type type);
+    }
+}
