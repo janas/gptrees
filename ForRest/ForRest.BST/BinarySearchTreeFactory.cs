9c4e58db08e371dc14ac5aba04ee82aaeffd35bf
Piotr Janaszek <janas03@yahoo.pl>
2011-12-08 00:46:36 +0100

    Added Factory for Binary Search Tree
    Class that creates an instance of Binary Search Tree and provides information about implemented plugin.

---
+++ ForRest/ForRest.BST/BinarySearchTreeFactory.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BinarySearchTreeFactory.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using ForRest.Provider.BLL;
+
+namespace ForRest.BST
+{
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class BinarySearchTreeFactory : ITreeFactory
+    {
+        public string GetPluginName()
+        {
+            return "Binary Search Tree Plugin";
+        }
+
+        public string GetPluginDescription()
+        {
+            return "Plugin that implements Binary Search Tree Algorithms";
+        }
+
+        public ITree GetTree(Type type)
+        {
+            object obj = (ITree)Activator.CreateInstance(typeof(BinarySearchTree<>).MakeGenericType(type));
+            var t = (ITree) obj;
+            return t;
+        }
+    }
+}
