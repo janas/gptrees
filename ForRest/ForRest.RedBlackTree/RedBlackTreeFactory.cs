4a0ee00dd4af1d7c87d096592c0ee3efb79e49c7
Piotr Janaszek <janas03@yahoo.pl>
2011-12-08 20:39:50 +0100

    Added initial implementation for Red-Black Trees

---
+++ ForRest/ForRest.RedBlackTree/RedBlackTreeFactory.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RedBlackTreeFactory.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using ForRest.Provider.BLL;
+
+namespace ForRest.RedBlackTree
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class RedBlackTreeFactory : ITreeFactory
+    {
+        public string GetPluginName()
+        {
+            return "Red-Black Tree Plugin";
+        }
+
+        public string GetPluginDescription()
+        {
+            return "Plugin that implements Red-Black Tree Algorithms";
+        }
+
+        public ITree<T> GetTree<T>()
+        {
+            object obj = Activator.CreateInstance(typeof(RedBlackTree<>).MakeGenericType(typeof(T)));
+            var t = (ITree<T>)obj;
+            return t;
+        }
+    }
+}
---
+++ ForRest/ForRest.RedBlackTree/RedBlackTreeNode.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RedBlackTreeNode.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using ForRest.Provider.BLL;
+
+namespace ForRest.RedBlackTree
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class RedBlackTreeNode<T> : Node<T>
+    {
+    }
+}
