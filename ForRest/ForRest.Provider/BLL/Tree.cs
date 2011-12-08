d6c096007ec59ea0fd862563b5128d7d7843b9f5
Piotr Janaszek <janas03@yahoo.pl>
2011-12-08 01:22:29 +0100

    Code cleanup

--- ForRest/ForRest.Provider/BLL/Tree.cs
+++ ForRest/ForRest.Provider/BLL/Tree.cs
@@ -9,10 +9,8 @@ namespace ForRest.Provider.BLL
     /// <summary>
     /// TODO: Update summary.
     /// </summary>
-    public abstract class Tree<T> : ITree
+    public abstract class Tree<T> : ITree<T>
     {
-        public abstract string GetPluginName();
-        public abstract string GetPluginDescription();
         public abstract void Clear();
         public abstract bool Contains(T data);
         public abstract void Add(T data);
