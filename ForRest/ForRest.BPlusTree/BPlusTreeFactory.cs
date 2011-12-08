db520356f911a7832159249d1242bffb919ad7b3
Piotr Janaszek <janas03@yahoo.pl>
2011-12-08 20:36:06 +0100

    Added initial implementation for B+ Trees

---
+++ ForRest/ForRest.BPlusTree/BPlusTreeFactory.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BPlusTreeFactory.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using ForRest.Provider.BLL;
+
+namespace ForRest.BPlusTree
+{
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class BPlusTreeFactory : ITreeFactory
+    {
+        public string GetPluginName()
+        {
+            return "B+ Tree Plugin";
+        }
+
+        public string GetPluginDescription()
+        {
+            return "Plugin that implements B+ Tree Algorithms";
+        }
+
+        public ITree<T> GetTree<T>()
+        {
+            object obj = Activator.CreateInstance(typeof(BPlusTree<>).MakeGenericType(typeof(T)));
+            var t = (ITree<T>)obj;
+            return t;
+        }
+    }
+}
---
+++ ForRest/ForRest.BPlusTree/BPlusTreeNode.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BPlusTreeNode.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using ForRest.Provider.BLL;
+
+namespace ForRest.BPlusTree
+{
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class BPlusTreeNode<T> : Node<T>
+    {
+    }
+}
