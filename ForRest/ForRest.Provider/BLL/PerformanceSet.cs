5cbeeb42d567efdddb199be0fa224587681de8af
Piotr Janaszek <janas03@yahoo.pl>
2011-12-08 01:20:28 +0100

    Added class for storing performance results

---
+++ ForRest/ForRest.Provider/BLL/PerformanceSet.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PerformanceSet.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ForRest.Provider.BLL
+{
+    
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class PerformanceSet
+    {
+        public string TypeOfTree { get; set; }
+        public string NoOfNodes { get; set; }
+        public string TypeOfNodes { get; set; }
+        public double SearchTime { get; set; }
+    }
+}
