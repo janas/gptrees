3d8fd531a5c7fe7dc47a9f32d44e832c7cdb4386
Piotr Janaszek <janas03@yahoo.pl>
2011-12-14 10:41:28 +0100

    Added TreeObject Class
    In this class trees of different types are stored.

---
+++ ForRest/ForRest.Provider/BLL/TreeObject.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ForRest.Provider.BLL
+{
+    public class TreeObject
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public readonly ITree<string> TextTree;
+        public readonly ITree<double> NumericTree;
+
+        public TreeObject(string name, string type, ITree<string> textTree)
+        {
+            Name = name;
+            Type = type;
+            TextTree = textTree;
+            NumericTree = null;
+        }
+
+        public TreeObject(string name, string type, ITree<double> numericTree)
+        {
+            Name = name;
+            Type = type;
+            TextTree = null;
+            NumericTree = numericTree;
+        }
+    }
+}
