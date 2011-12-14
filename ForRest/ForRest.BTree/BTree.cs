df331ed593f126fbbd3736cbf9c28aad86cffd39
Piotr Janaszek <janas03@yahoo.pl>
2011-12-14 11:17:46 +0100

    Added B Tree implementation

--- ForRest/ForRest.BTree/BTree.cs
+++ ForRest/ForRest.BTree/BTree.cs
@@ -5,24 +5,158 @@ namespace ForRest.BTree
 {
     public class BTree<T> : Tree<T>
     {
+        private BTreeNode<T> _root;
+        private IComparer<T> _comparer = Comparer<T>.Default;
+        private int _M;
+
+        public BTree(int degree)
+        {
+            _root = null;
+            _M = degree;
+        }
+
+        public int M
+        {
+            get { return _M; }
+        }
+
+        public override Node<T> Root
+        {
+            get { return _root; }
+            set { _root = (BTreeNode<T>) value; }
+        }
+
         public override void Clear()
         {
-            throw new System.NotImplementedException();
+            _root = null;
         }
 
         public override List<int> Contains(T data)
         {
-            throw new System.NotImplementedException();
+            List<int> path = new List<int>();
+            BTreeNode<T> current = _root;
+            while (current != null)
+            {
+                for (int i = 0; i < current.Values.Count; i++)
+                {
+                    int result = _comparer.Compare(current.Values[i], data);
+                    if (result == 0)
+                        return path;
+                    else if (result > 0)
+                    {
+                        if (current.Neighbors == null)
+                            return null;
+                        current = (BTreeNode<T>)current.Neighbors[i];
+                        path.Add(i);
+                    }
+                    else
+                    {
+                        if (i + 1 == current.Values.Count)
+                        {
+                            if (current.Neighbors == null)
+                                return null;
+                            current = (BTreeNode<T>)current.Neighbors[i + 1];
+                            path.Add(i + 1);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        private BTreeNode<T> Insert(BTreeNode<T> node, T data)
+        {
+            if (!node.isLeaf)
+            {
+                // Look for child to go to
+                for (int i = 0; i < node.Values.Count; i++)
+                {
+                    int result = _comparer.Compare(node.Values[i], data);
+                    if (result > 0)
+                        return Insert(node.ChildAt(i), data);
+                    else
+                        if (i + 1 == node.Values.Count)
+                            return Insert(node.ChildAt(i + 1), data);
+                }
+                return null;
+            }
+            else
+            {
+                if (!node.IsFull)
+                    return node.Add(data);
+                else
+                {
+                    //return node.Split(data);
+                    if (node == _root)
+                    {
+                        _root = node.Split(data);
+                        return _root;
+                    }
+                    else
+                    {
+                        node.Parent = node.Split(data);
+                        return node.Parent;
+                    }
+                }
+            }
         }
 
         public override void Add(T data)
         {
-            throw new System.NotImplementedException();
+            if (_root == null)
+            {
+                List<T> dataList = new List<T>();
+                dataList.Add(data);
+                BTreeNode<T> node = new BTreeNode<T>(_M, null, dataList);
+                _root = node;
+            }
+            else
+            {
+                BTreeNode<T> node = Insert(_root, data);
+                while (node.Parent != null)
+                    node = node.Parent;
+                _root = node;
+            }
+        }
+
+        private bool Delete(BTreeNode<T> node, T data)
+        {
+            while (node != null)
+            {
+                for (int i = 0; i < node.Values.Count; i++)
+                {
+                    int result = _comparer.Compare(node.Values[i], data);
+                    if (result == 0)
+                        return node.Delete(data, i);
+                    else if (result > 0)
+                    {
+                        if (node.Neighbors == null)
+                            return false;
+                        node = (BTreeNode<T>)node.Neighbors[i];
+                        return Delete(node, data);
+                    }
+                    else
+                    {
+                        if (i + 1 == node.Values.Count)
+                        {
+                            if (node.Neighbors == null)
+                                return false;
+                            node = (BTreeNode<T>)node.Neighbors[i + 1];
+                            return Delete(node, data);
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
         public override bool Remove(T data)
         {
-            throw new System.NotImplementedException();
+            List<int> path = new List<int>();
+            if (_root == null)
+                return false;
+            else
+                return Delete(_root, data);
         }
     }
 }
--- ForRest/ForRest.BTree/BTreeNode.cs
+++ ForRest/ForRest.BTree/BTreeNode.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Collections.Generic;
 using ForRest.Provider.BLL;
 
 namespace ForRest.BTree
@@ -14,6 +15,328 @@ namespace ForRest.BTree
     /// </summary>
     public class BTreeNode<T> : Node<T>
     {
+        private BTreeNode<T> _parent;
+        private bool _isLeaf;
+        private readonly int _M;
+        private readonly IComparer<T> _comparer = Comparer<T>.Default;
 
+        public BTreeNode<T> Parent
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        /// <summary>
+        /// Indicates whether BTreeNode is a leaf
+        /// </summary>
+        public bool isLeaf
+        {
+            set { _isLeaf = value; }
+            get { return _isLeaf; }
+        }
+
+        /// <summary>
+        /// Indicates whether BTreeNode has maximal nuber of values
+        /// </summary>
+        public bool IsFull
+        {
+            get
+            {
+                if (Values.Count < _M * 2)
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether BTreeNode has minimal nuber of values
+        /// </summary>
+        public bool IsHalf
+        {
+            get
+            {
+                if (Values.Count > _M)
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="degree">Degree of BTree in Knuth's notation</param>
+        /// <param name="parent">Parent node</param>
+        /// <param name="data">Values for the node</param>
+        public BTreeNode(int degree, BTreeNode<T> parent, List<T> data)
+            : base(data, null)
+        {
+            _parent = parent;
+            Values = data;
+            Neighbors = null;
+            _isLeaf = true;
+            _M = degree;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="degree">Degree of BTree in Knuth's notation</param>
+        /// <param name="parent">Parent node</param>
+        /// <param name="data">Values for the node</param>
+        /// <param name="children">Child nodes</param>
+        public BTreeNode(int degree, BTreeNode<T> parent, List<T> data, NodeList<T> children)
+        {
+            _parent = parent;
+            Values = data;
+            Neighbors = children;
+            _isLeaf = true;
+            _M = degree;
+            for (int i = 0; i < Neighbors.Count; i++)
+            {
+                ((BTreeNode<T>)Neighbors[i]).Parent = this;
+            }
+        }
+
+        /// <summary>
+        /// Returns i-th child node
+        /// </summary>
+        /// <param name="i">Index of the child node</param>
+        /// <returns>Child node of index i</returns>
+        public BTreeNode<T> ChildAt(int i)
+        {
+            return (BTreeNode<T>)Neighbors[i];
+        }
+
+        /// <summary>
+        /// Adds input node values and child nodes 
+        /// </summary>
+        /// <param name="node">Input node</param>
+        public BTreeNode<T> Add(BTreeNode<T> node)
+        {
+            if (node.Values.Count != 1 || node.Neighbors.Count != 2)
+                throw new System.NotImplementedException();
+            T nodeData = node.Values[0];
+            BTreeNode<T> rightNode = (BTreeNode<T>)node.Neighbors[1];
+            BTreeNode<T> leftNode = (BTreeNode<T>)node.Neighbors[0];
+            rightNode.Parent = this;
+            leftNode.Parent = this;
+            for (int m = 0; m < Values.Count; m++)
+            {
+                int result = _comparer.Compare(Values[m], nodeData);
+                if (result > 0)
+                {
+                    if (IsFull)
+                    {
+                        Values.Insert(m, nodeData);
+                        Neighbors.RemoveAt(m);
+                        Neighbors.Insert(m, rightNode);
+                        Neighbors.Insert(m, leftNode);
+                        return Split();
+                    }
+                    else
+                    {
+                        Values.Insert(m, nodeData);
+                        Neighbors.RemoveAt(m);
+                        Neighbors.Insert(m, rightNode);
+                        Neighbors.Insert(m, leftNode);
+                        break;
+                    }
+                }
+                else
+                    if (m + 1 == Values.Count)
+                    {
+                        if (IsFull)
+                        {
+                            Values.Add(nodeData);
+                            Neighbors.RemoveAt(m + 1);
+                            Neighbors.Add(leftNode);
+                            Neighbors.Add(rightNode);
+                            return Split();
+                        }
+                        else
+                        {
+                            Values.Add(nodeData);
+                            Neighbors.RemoveAt(m + 1);
+                            Neighbors.Add(leftNode);
+                            Neighbors.Add(rightNode);
+                            break;
+                        }
+                    }
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Adds input item to list of values
+        /// </summary>
+        /// <param name="data">Input item</param>
+        public BTreeNode<T> Add(T data)
+        {
+            for (int i = 0; i < Values.Count; i++)
+            {
+                int result = _comparer.Compare(Values[i], data);
+                if (result > 0)
+                {
+                    Values.Insert(i, data);
+                    return this;
+                }
+                else
+                    if (i + 1 == Values.Count)
+                    {
+                        Values.Add(data);
+                        return this;
+                    }
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Splits node 
+        /// </summary>
+        public BTreeNode<T> Split()
+        {
+            T centerData;
+            List<T> leftData = new List<T>();
+            List<T> rightData = new List<T>();
+            NodeList<T> leftNeighbors = new NodeList<T>(0);
+            NodeList<T> rightNeighbors = new NodeList<T>(0);
+            int i, j;
+            for (i = 0; i < Values.Count / 2; i++)
+            {
+                leftData.Add(Values[i]);
+                leftNeighbors.Add(Neighbors[i]);
+            }
+            centerData = Values[i];
+            leftNeighbors.Add(Neighbors[i]);
+            for (j = ++i; j < Values.Count; j++)
+            {
+                rightData.Add(Values[j]);
+                rightNeighbors.Add(Neighbors[j]);
+            }
+            rightNeighbors.Add(Neighbors[j]);
+
+            Values.Clear();
+            BTreeNode<T> leftNode = new BTreeNode<T>(_M, this.Parent, leftData, leftNeighbors);
+            leftNode.isLeaf = false;
+            BTreeNode<T> rightNode = new BTreeNode<T>(_M, this.Parent, rightData, rightNeighbors);
+            rightNode.isLeaf = false;
+
+            List<T> centerDataList = new List<T>();
+            centerDataList.Add(centerData);
+            NodeList<T> children = new NodeList<T>(0);
+            children.Add(leftNode);
+            children.Add(rightNode);
+            BTreeNode<T> centerNode = new BTreeNode<T>(_M, null, centerDataList, children);
+            centerNode.isLeaf = false;
+
+            if (_parent != null)
+            {
+                leftNode.Parent = _parent;
+                rightNode.Parent = _parent;
+                _parent.isLeaf = false;
+                //centerNode.Parent = _parent.Parent;
+                _parent = _parent.Add(centerNode);
+                return _parent;
+            }
+            else
+            {
+                leftNode.Parent = centerNode;
+                rightNode.Parent = centerNode;
+                return centerNode;
+            }
+        }
+
+        /// <summary>
+        /// Splits node 
+        /// </summary>
+        /// <param name="data">Input item forcing the split</param>
+        public BTreeNode<T> Split(T data)
+        {
+            T centerData;
+            List<T> leftData = new List<T>();
+            List<T> rightData = new List<T>();
+            int centerResult, leftResult, rightResult;
+            int i, j;
+            for (i = 0; i < Values.Count / 2; i++)
+                leftData.Add(Values[i]);
+            centerData = Values[i];
+            centerResult = _comparer.Compare(Values[i], data);
+            for (j = ++i; j < Values.Count; j++)
+                rightData.Add(Values[j]);
+            if (centerResult > 0)
+                for (int k = 0; k < leftData.Count; k++)
+                {
+                    leftResult = _comparer.Compare(leftData[k], data);
+                    if (leftResult > 0)
+                    {
+                        leftData.Insert(k, data);
+                        break;
+                    }
+                    else
+                    {
+                        if (k + 1 == leftData.Count)
+                        {
+                            leftData.Insert(k + 1, data);
+                            break;
+                        }
+                    }
+                }
+            else
+                for (int l = 0; l < rightData.Count; l++)
+                {
+                    rightResult = _comparer.Compare(rightData[l], data);
+                    if (rightResult > 0)
+                    {
+                        rightData.Insert(l, data);
+                        break;
+                    }
+                    else
+                    {
+                        if (l + 1 == rightData.Count)
+                        {
+                            rightData.Insert(l + 1, data);
+                            break;
+                        }
+                    }
+                }
+            Values.Clear();
+            BTreeNode<T> leftNode = new BTreeNode<T>(_M, this.Parent, leftData);
+            BTreeNode<T> rightNode = new BTreeNode<T>(_M, this.Parent, rightData);
+
+            List<T> centerDataList = new List<T>();
+            centerDataList.Add(centerData);
+            NodeList<T> children = new NodeList<T>(0);
+            children.Add(leftNode);
+            children.Add(rightNode);
+            BTreeNode<T> centerNode = new BTreeNode<T>(_M, null, centerDataList, children);
+            centerNode.isLeaf = false;
+
+            if (_parent != null)
+            {
+                leftNode.Parent = _parent;
+                rightNode.Parent = _parent;
+                _parent.isLeaf = false;
+                _parent = _parent.Add(centerNode);
+                return _parent;
+            }
+            else
+            {
+                leftNode.Parent = centerNode;
+                rightNode.Parent = centerNode;
+                return centerNode;
+            }
+        }
+
+        public bool Delete(T data, int index)
+        {
+            if (isLeaf && !IsHalf)
+            {
+                Values.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
     }
 }
