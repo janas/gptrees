bb9f5b361e666be76e867e173048f45309954b83
Piotr Janaszek <janas03@yahoo.pl>
2011-12-27 03:23:52 +0100

    Added ResultsSet form to display results of searching

---
+++ ForRest/ForRest/ResultsSet.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ForRest
+{
+    partial class ResultsSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResultsSet));
+            this.dataGridViewResultsSet = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultsSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewResultsSet
+            // 
+            this.dataGridViewResultsSet.AllowUserToAddRows = false;
+            this.dataGridViewResultsSet.AllowUserToDeleteRows = false;
+            this.dataGridViewResultsSet.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResultsSet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewResultsSet.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewResultsSet.Name = "dataGridViewResultsSet";
+            this.dataGridViewResultsSet.ReadOnly = true;
+            this.dataGridViewResultsSet.Size = new System.Drawing.Size(544, 262);
+            this.dataGridViewResultsSet.TabIndex = 0;
+            // 
+            // ResultsSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 262);
+            this.Controls.Add(this.dataGridViewResultsSet);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "ResultsSet";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Results Set";
+            this.Load += new System.EventHandler(this.ResultsSetLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResultsSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewResultsSet;
+    }
+}
---
+++ ForRest/ForRest/ResultsSet.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ForRest
+{
+    public partial class ResultsSet : Form
+    {
+        private readonly Provider.Provider _provider;
+
+        public ResultsSet(Provider.Provider provider)
+        {
+            InitializeComponent();
+            _provider = provider;
+            dataGridViewResultsSet.ColumnHeadersBorderStyle = ProperColumnHeadersBorderStyle;
+        }
+
+        private static DataGridViewHeaderBorderStyle ProperColumnHeadersBorderStyle
+        {
+            get
+            {
+                return (SystemFonts.MessageBoxFont.Name == "Segoe UI")
+                           ? DataGridViewHeaderBorderStyle.None
+                           : DataGridViewHeaderBorderStyle.Raised;
+            }
+        }
+
+        private void ResultsSetLoad(object sender, System.EventArgs e)
+        {
+            dataGridViewResultsSet.DataSource = _provider.PerformanceSets;
+            //dataGridViewResultsSet.DataSource = _provider.BatchPerformanceSet;
+            dataGridViewResultsSet.Columns[0].HeaderText = "Tree name";
+            dataGridViewResultsSet.Columns[1].HeaderText = "Type of the tree";
+            dataGridViewResultsSet.Columns[2].HeaderText = "Number of nodes";
+            dataGridViewResultsSet.Columns[3].HeaderText = "Nodes type";
+            dataGridViewResultsSet.Columns[4].HeaderText = "Search time (in ms)";
+        }
+    }
+}
