e1ef3f709f9d12ead1e96e7c229ab3f4bf24bcf0
Piotr Janaszek <janas03@yahoo.pl>
2011-12-27 03:25:30 +0100

    Modified to display Loader form

--- ForRest/ForRest/Program.cs
+++ ForRest/ForRest/Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ForRest
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            LoadApplication();
+        }
+
+        private static void LoadApplication()
+        {
+            string providerDll = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "ForRest.Provider.dll");
+            string gleeDll = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Microsoft.GLEE.dll");
+            string gleeDrawingDll = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath),
+                                                 "Microsoft.GLEE.Drawing.dll");
+            string gleeDrawingGDIdll = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath),
+                                                    "Microsoft.GLEE.GraphViewerGDI.dll");
+            if (File.Exists(providerDll) && File.Exists(gleeDll) && File.Exists(gleeDrawingDll) &&
+                File.Exists(gleeDrawingGDIdll))
+            {
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                Application.Run(new Loader());
+            }
         }
     }
 }
