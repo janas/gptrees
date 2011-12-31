cce2576f9ffe6554310ee8aea43da1e282ce9164
Piotr Janaszek <janas03@yahoo.pl>
2011-12-31 01:08:03 +0100

    Added user controls responsible for drawing nodes and edges

---
+++ ForRest/ForRest/UserControlEdge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ForRest
+{
+    public partial class UserControlEdge : UserControl
+    {
+        bool LeftToRight;
+        bool _mark;
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020; //WS_EX_TRANSPARENT
+                return cp;
+            }
+        }
+
+        public UserControlEdge(bool ltr, bool mark)
+        {
+            InitializeComponent();
+            LeftToRight = ltr;
+            _mark = mark;
+        }
+
+        private void UserControlEdge_Paint(object sender, PaintEventArgs e)
+        {
+            BackColor = Color.Transparent;
+            Pen pen;
+            if (_mark)
+                pen = new Pen(Color.Red, 1);
+            else
+                pen = new Pen(Color.Black, 1);
+            Graphics g = CreateGraphics();
+            if (LeftToRight)
+                g.DrawLine(pen, 0, 0,//Location.X, Location.Y,
+                    Width - 1, Height - 1);
+            else
+                g.DrawLine(pen, Width - 1, 0,
+                    0, Height - 1);
+        }
+    }
+}
---
+++ ForRest/ForRest/UserControlNode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ForRest
+{
+    public partial class UserControlNode : UserControl
+    {
+        private List<string> Values;
+        public Rectangle MyArea;
+        public Font font;
+        public bool _mark;
+
+        public UserControlNode(List<string> values, Rectangle myArea, bool mark)
+        {
+            InitializeComponent();
+            Values = values;
+            MyArea = myArea;
+            _mark = mark;
+            font = new Font("Tahoma", 10);
+        }
+
+        public Rectangle GetMyArea()
+        {
+            return MyArea;
+        }
+
+        public void verifySize()
+        {
+            Graphics g = CreateGraphics();
+            string text = "";
+            for (int i = 0; i < Values.Count; i++)
+                text += Values[i].ToString() + " | ";
+            SizeF textRect = g.MeasureString(text, font);
+            if (Width > (int)textRect.Width * 2)
+            {
+                int centerX = Location.X + Width / 2;
+                Width = (int)textRect.Width * 2;
+                Location = new Point(centerX - Width / 2, Location.Y);
+            }
+            g.Dispose();
+        }
+
+        private void UserControlNode_Paint(object sender, PaintEventArgs e)
+        {
+            BackColor = Color.White;
+            Pen pen;
+            if (_mark)
+                pen = new Pen(Color.Red, 1);
+            else
+                pen = new Pen(Color.Black, 1);
+            Graphics g = CreateGraphics();
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            g.DrawRectangle(pen, new Rectangle(0, 0, Width - 1, Height - 1));
+            for (int i = 0; i < Values.Count; i++)
+            {
+                g.DrawString(Values[i].ToString(), font, brush,
+                    new RectangleF( i * (Width) / Values.Count + 2, 2, 
+                        (Width / Values.Count) - 5, Height - 5));
+                if (i != 0)
+                    g.DrawLine(pen, i * (Width) / Values.Count, 0, 
+                        i * (Width) / Values.Count, Height-1);
+            }
+        }
+
+        private void UserControlNode_MouseHover(object sender, EventArgs e)
+        {
+            string result = "";
+            for (int i = 0; i < Values.Count; i++)
+            {
+                if (i != 0)
+                    result += " | ";
+                result += Values[i].ToString();
+            }
+            nodeToolTip.Show(result, this);
+        }
+    }
+}
